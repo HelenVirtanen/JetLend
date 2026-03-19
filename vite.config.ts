3ed766647e1f72ac0cc7328f6bb227ce480d2b06
Helen Virtanen <helenvirtanen28@gmail.com>
2026-03-19 15:29:47 +0300

    chore: edit repo path to JetLend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'jetlend';
+const repoName = 'JetLend';
 
 // https://vite.dev/config/
 export default defineConfig({
