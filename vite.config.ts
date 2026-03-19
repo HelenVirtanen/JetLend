348d5354d38995525efeaa8599bd8d66fd2b2ee8
Helen Virtanen <helenvirtanen28@gmail.com>
2026-03-19 15:12:03 +0300

    feature: add Github Pages deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'JetLend';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
   test: {
     include: ["src/**/*.test.{ts,tsx}"],
     environment: "jsdom",
